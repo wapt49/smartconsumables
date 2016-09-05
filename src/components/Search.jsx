8f4b9f03fd098090f904fe203c6dba5adc640392
Widia Ahadi Putra <wputra@itm-consulting.de>
2016-09-05 09:29:51 +0200

    Init

---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class Search extends Component{
+    render(){
+        return(
+            <div>
+                MY APP
+            </div>
+        )
+    }
+}
+
+export default Search
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry:[
+        './src/index.js'
+    ],
+    output:{
+        path: __dirname,
+        filename: 'app/js/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    query: {
+        presets:['react']
+     }
+}
